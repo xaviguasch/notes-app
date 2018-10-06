bb41166884d22c2f1df8801550c24d8871f7851b
xaviguasch <xaviguasch@gmail.com>
2018-10-06 19:33:32 +0200

    more arrow function substitutions

--- notes-edit.js
+++ notes-edit.js
@@ -17,9 +17,7 @@ const noteId = location.hash.substring(1)
 
 let notes = getSavedNotes()
 
-let note = notes.find(function (note) {
-  return note.id === noteId
-})
+let note = notes.find((note) => note.id === noteId)
 
 
 
@@ -36,21 +34,21 @@ dateElement.textContent = generateLastEdited(note.updatedAt)
 
 
 
-titleElement.addEventListener('input', function (e) {
+titleElement.addEventListener('input', (e) => {
   note.title = e.target.value
   note.updatedAt = moment().valueOf()
   dateElement.textContent = generateLastEdited(note.updatedAt)
   saveNotes(notes)
 })
 
-bodyElement.addEventListener('input', function (e) {
+bodyElement.addEventListener('input', (e) => {
   note.body = e.target.value
   note.updatedAt = moment().valueOf()
   dateElement.textContent = generateLastEdited(note.updatedAt)
   saveNotes(notes)
 })
 
-removeElement.addEventListener('click', function (e) {
+removeElement.addEventListener('click', (e) => {
   removeNote(note.id)
   saveNotes(notes)
   location.assign('/index.html')
@@ -58,13 +56,11 @@ removeElement.addEventListener('click', function (e) {
 
 
 
-window.addEventListener('storage', function (e) {
+window.addEventListener('storage', (e) => {
   if (e.key === 'notes') {
     notes = JSON.parse(e.newValue)
 
-    note = notes.find(function (note) {
-      return note.id === noteId
-    })
+    note = notes.find((note) => note.id === noteId)
     
     
     
--- notes-functions.js
+++ notes-functions.js
@@ -123,6 +123,4 @@ const renderNotes = (notes, filters) => {
 
 
 // Generate the last edited message
-const generateLastEdited = function (timestamp) {
-  return `Last edited ${moment(timestamp).fromNow()}`
-}
+const generateLastEdited = (timestamp) => `Last edited ${moment(timestamp).fromNow()}`
